2a7c461ba375dde5d89f4a95ec50e791af37619e
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-06-09 09:00:42 -0700

    started to write tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,13 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+    it('handles the / route', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                //res.body -> json
+                //res.body -> text
+                expect(res.text).toEqual('Hello!');
+         });
+    })
 });
