df2fb4392c8bc7140469ee978148197eaf1d32fc
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-06-09 00:24:17 -0700

    created html and json response

--- server.js
+++ server.js
@@ -12,19 +12,39 @@ const server = net.createServer(client => {
     console.log(data.toString()); 
     //received data and convert to a string 
     
-    const body = JSON.stringify({
-      name: 'Spot',
-      age: 5,
-      weight: '20 lbs'
-    });
+    const [ , path] = data.toString().split(' ');
+    //if I got to '/' send an h1 with hello
+    if(path ==='/') {
+      const body = '<h1>Hello!<h1>';
+    
+      //res.send(body) constructed response below
+      client.end(`HTTP/1.1 200 OK
+    Date: Mon, 08 June 2020 23:40 GMT
+    Content-Length: ${body.length}
+    Context-Type: text/html
+    
+    ${body}`);
+        //send a response the browser understands
+      }
 
-    client.end(`HTTP/1.1 200 OK
-Date: Mon, 08 June 2020 23:40 GMT
-Content-Length: ${body.length}
-Context-Type: application/json
+    //if I got to '/dogs' send json with dog
+    else if(path ==='dogs') {
+      const body = JSON.stringify({
+        name: 'Spot',
+        age: 5,
+        weight: '20 lbs'
+      });
 
-${body}`);
-    //send a response the browser understands
+      
+      //res.json(body) constructed response below
+      client.end(`HTTP/1.1 200 OK
+      Date: Mon, 08 June 2020 23:40 GMT
+      Content-Length: ${body.length}
+      Context-Type: application/json
+      
+      ${body}`);
+    }
+      //send a response the browser understands
   });
 });
 
