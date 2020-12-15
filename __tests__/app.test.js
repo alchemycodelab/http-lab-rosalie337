bc12adca10bd0c818197545641e2ac0b1c1cd559
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-12-14 20:17:45 -0800

    all tests are passing in app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,34 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  // it('handles the / route', () => {
-  //   return request(app)
-  //     .get('/')
-  //     .then(res => {
-  //       //res.body -> json
-  //       //res.body -> text
-  //       expect(res.text).toEqual('Hello!');
-  //     });
-  // });
+  it('should GET hi', async() => {
+    const res = await request(app)
+      .get('/');
+    expect(res.text).toEqual('hi');
+  });
+
+  it('should POST status code 200 in plain textq with request body ', async() => {
+    const res = await request(app)
+      .post('/echo')
+      .send('hi');
+    expect(res.text).toEqual('hi');
+  });
+
+  it('should return Header that reads red', async() => {
+    const res = await request(app)
+      .get('/red');
+    expect(res.text).toEqual('<h1>red</h1>');
+
+  });
+  it('should return Header that reads green', async() => {
+    const res = await request(app)
+      .get('/green');
+    expect(res.text).toEqual('<h1>green</h1>');
+
+  });
+  it('should return Header that reads blue', async() => {
+    const res = await request(app)
+      .get('/blue');
+    expect(res.text).toEqual('<h1>blue</h1>');
+  });
 });
